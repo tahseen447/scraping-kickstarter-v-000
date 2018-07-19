6c00413053a3d2c507e2e4138008b2058ef4b9d9
tahseen447 Siddiqui <tahseenfatima1986@gmail.com>
2018-07-19 19:06:31 +0000

    automatically backed up by learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -14,7 +14,9 @@ def create_project_hash
   projects = {}
 
   kickstarter.css("li.project.grid_4").each do |project|
-    projects[project] = {}
+    title =
+
+    projects[title.to_sym] = {}
   end
 
   projects
